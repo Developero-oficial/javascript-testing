cd9a9a0532ac90e2a2a7e52be7ac68b05451e555
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-05-07 19:11:08 -0500

    added mock behavior

--- jest-mocks-fundamentals/number-facts/index.js
+++ jest-mocks-fundamentals/number-facts/index.js
@@ -1,7 +1,6 @@
 import { getRandomNumberFactService } from "./service";
 
 export const getRandomNumberFact = async () => {
-  console.log(getRandomNumberFactService);
   const randomNumberFact = await getRandomNumberFactService();
   return randomNumberFact;
 };
--- jest-mocks-fundamentals/number-facts/number-facts.test.js
+++ jest-mocks-fundamentals/number-facts/number-facts.test.js
@@ -1,8 +1,14 @@
 import { getRandomNumberFact } from "./";
+import { getRandomNumberFactService } from "./service";
 
 jest.mock("./service");
 
 test("should return a random number fact", async () => {
+  getRandomNumberFactService.mockReturnValueOnce({
+    text:
+      "158 is the year that the earliest dated use of Sol invictus, in a dedication from Rome.",
+  });
+
   const numberFact = await getRandomNumberFact();
 
   expect(numberFact.text).toBe(
