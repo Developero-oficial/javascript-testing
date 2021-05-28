0f5eaeee41a47c8f6d9d6557d0f605bb62b19e96
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-05-28 17:56:57 -0500

    init server

--- backend-testing/src/server.js
+++ backend-testing/src/server.js
@@ -1,1 +1,14 @@
-const hello = 'hello world'
+require('dotenv').config()
+const express = require('express')
+const pino = require('pino-http')()
+const logger = require('pino')()
+
+const app = express()
+
+app.use(pino)
+
+const port = process.env.PORT
+
+app.listen(port, () => {
+  logger.info(`server listening on port ${port}`)
+})
