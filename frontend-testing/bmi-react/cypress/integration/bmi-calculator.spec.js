5434613593c0777fd323e2123cf755f5d41b2f18
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-05-28 13:58:10 -0500

    finished cypress tests

--- frontend-testing/bmi-react/cypress/integration/bmi-calculator.spec.js
+++ frontend-testing/bmi-react/cypress/integration/bmi-calculator.spec.js
@@ -10,4 +10,40 @@ describe('bmi calculator', () => {
     cy.findByText(/bmi: 15.432/i).should('be.visible')
     cy.findByText(/bmi estimation: thiness/i).should('be.visible')
   })
+
+  it('calculate Normal result', () => {
+    cy.visit('/')
+
+    cy.findByLabelText('Weight (KG)').type('80')
+    cy.findByLabelText('Height (M)').type('1.8')
+
+    cy.get('button').click()
+
+    cy.findByText(/bmi: 24.691/i).should('be.visible')
+    cy.findByText(/bmi estimation: normal/i).should('be.visible')
+  })
+
+  it('calculate Overweight result', () => {
+    cy.visit('/')
+
+    cy.findByLabelText('Weight (KG)').type('90')
+    cy.findByLabelText('Height (M)').type('1.8')
+
+    cy.get('button').click()
+
+    cy.findByText(/bmi: 27.778/i).should('be.visible')
+    cy.findByText(/bmi estimation: overweight/i).should('be.visible')
+  })
+
+  it('calculate Obese result', () => {
+    cy.visit('/')
+
+    cy.findByLabelText('Weight (KG)').type('100')
+    cy.findByLabelText('Height (M)').type('1.8')
+
+    cy.get('button').click()
+
+    cy.findByText(/bmi: 30.864/i).should('be.visible')
+    cy.findByText(/bmi estimation: obese/i).should('be.visible')
+  })
 })
