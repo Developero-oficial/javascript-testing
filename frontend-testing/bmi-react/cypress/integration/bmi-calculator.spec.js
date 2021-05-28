8dd43b5b25e09c290601badbf8d223310de012b6
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-05-28 13:34:48 -0500

    added first cypress test

---
+++ frontend-testing/bmi-react/cypress/integration/bmi-calculator.spec.js
@@ -0,0 +1,16 @@
+describe('bmi calculator', () => {
+  it('calculate Thiness result', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('#weight').type('50')
+    cy.get('#height').type('1.8')
+
+    cy.get('button').click()
+
+    cy.get('.App > :nth-child(2)').should('have.text', 'BMI: 15.432')
+    cy.get('.App > :nth-child(3)').should(
+      'have.text',
+      'BMI Estimation: Thiness',
+    )
+  })
+})
