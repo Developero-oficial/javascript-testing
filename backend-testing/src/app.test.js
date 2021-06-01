80ec945aed7b41c4508aa9a08918c9770765c0bf
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-05-31 19:32:06 -0500

    added buildUser fixture

--- backend-testing/src/app.test.js
+++ backend-testing/src/app.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const app = require('./app')
 const { getUsers, addUser } = require('./data/users')
+const { buildUser } = require('./__fixtures__/users')
 
 jest.mock('./data/users')
 
@@ -13,7 +14,7 @@ describe('users', () => {
   test('should store a user', async () => {
     const result = await request(app)
       .post('/users')
-      .send({ name: 'john', address: 'fake address', age: '20', uid: 'abc' })
+      .send(buildUser())
       .set('Accept', 'application/json')
       .expect(201)
 
@@ -21,6 +22,14 @@ describe('users', () => {
   })
 
   test('should get all users', async () => {
+    const user = buildUser()
+    getUsers.mockReturnValue([user])
+    const result = await request(app).get('/users').expect(200)
+
+    expect(result.body).toEqual([user])
+  })
+
+  test('should get empty users when there are no users', async () => {
     getUsers.mockReturnValue([])
     const result = await request(app).get('/users').expect(200)
 
