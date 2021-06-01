c094a23d30f0bbfcd67c4a429ad292a51d33dbb8
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-05-31 19:44:59 -0500

    get and remove user by uid addded

--- backend-testing/src/app.test.js
+++ backend-testing/src/app.test.js
@@ -1,6 +1,12 @@
 const request = require('supertest')
 const app = require('./app')
-const { getUsers, addUser, updateUserByUid } = require('./data/users')
+const {
+  getUsers,
+  addUser,
+  updateUserByUid,
+  findUserByUid,
+  removeUserByUid,
+} = require('./data/users')
 const { buildUser } = require('./__fixtures__/users')
 
 jest.mock('./data/users')
@@ -9,6 +15,8 @@ beforeEach(() => {
   getUsers.mockReset()
   addUser.mockReset()
   updateUserByUid.mockReset()
+  findUserByUid.mockReset()
+  removeUserByUid.mockReset()
 })
 
 describe('users', () => {
@@ -48,5 +56,21 @@ describe('users', () => {
       .expect(200)
 
     expect(result.body).toEqual([user])
+  })
+
+  test('should get an user by uid', async () => {
+    const user = buildUser()
+    findUserByUid.mockReturnValue(user)
+    const result = await request(app).get(`/users/${user.uid}`).expect(200)
+
+    expect(result.body).toEqual(user)
+  })
+
+  test('should delete an user by uid', async () => {
+    const user = buildUser()
+    removeUserByUid.mockReturnValue([])
+    const result = await request(app).delete(`/users/${user.uid}`).expect(200)
+
+    expect(result.body).toEqual([])
   })
 })
