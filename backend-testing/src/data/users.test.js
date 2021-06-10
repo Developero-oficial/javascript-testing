e351b16c1dc0045ce499fd5afb6aec5fadf0ed11
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-06-10 07:50:22 -0500

    added findUserByUid test

--- backend-testing/src/data/users.test.js
+++ backend-testing/src/data/users.test.js
@@ -1,8 +1,19 @@
-const { addUser, getUsers } = require('./users')
+const { addUser, getUsers, findUserByUid } = require('./users')
 const { buildUser } = require('../__fixtures__/users')
 
 test('should add new user', () => {
   const user = buildUser()
   addUser(user)
   expect(getUsers()).toEqual([user])
+})
+
+test('findUserByUid - should return undefined when there are no users', () => {
+  const user = findUserByUid({ uid: '' })
+  expect(user).toBe(undefined)
+})
+
+test('findUserByUid - should return a valid user', () => {
+  const user = buildUser()
+  const userFound = findUserByUid({ uid: user.uid })
+  expect(userFound).toEqual(user)
 })
