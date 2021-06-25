8263212059d7d3d97f87d68ba1163452a850c767
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-06-25 12:40:42 -0500

    setup mongoose

--- backend-testing-2/src/server.js
+++ backend-testing-2/src/server.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 const app = require('./app')
+const { connectDb } = require('./db/mongo')
 
 const logger = require('pino')()
 
 const port = process.env.PORT
+const uri = process.env.URI
 
-app.listen(port, () => {
-  logger.info(`server listening on port ${port}`)
+connectDb({ uri }).then(() => {
+  logger.info('Db Connected')
+  app.listen(port, () => {
+    logger.info(`server listening on port ${port}`)
+  })
 })
