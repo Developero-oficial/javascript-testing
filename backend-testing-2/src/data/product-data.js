9c7d42eb8159feff450bb8cca910088289e266d3
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-06-25 12:59:35 -0500

    added product-data

---
+++ backend-testing-2/src/data/product-data.js
@@ -0,0 +1,11 @@
+const Product = require('../models/product-model')
+
+module.exports.saveProduct = ({ name, size, description }) => {
+  const product = new Product({
+    name,
+    size,
+    description,
+  })
+
+  return product.save()
+}
