5f757a39faf87afd29b7dbff7b88528ecae6f151
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-06-26 15:11:47 -0500

    added integration get products

--- backend-testing-2/__tests__/products.integration.test.js
+++ backend-testing-2/__tests__/products.integration.test.js
@@ -14,6 +14,14 @@ beforeAll(async () => {
   await connectDb({ uri: mongoUri })
 })
 
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections()
+
+  for (let collection of collections) {
+    await collection.deleteMany()
+  }
+})
+
 afterAll(async () => {
   await mongoose.disconnect()
   await mongoServer.stop()
@@ -34,11 +42,37 @@ describe('products integration tests', () => {
 
     const { name, size, description, _id } = response.body.productStored
 
-    console.log(response.body)
-
     expect(name).toBe(product.name)
     expect(size).toBe(product.size)
     expect(description).toBe(product.description)
     expect(_id).toBeTruthy()
+  })
+
+  test('GET /products empty values', async () => {
+    const response = await request(app).get('/products').expect(200)
+
+    expect(response.body).toEqual({ products: [] })
+  })
+
+  test('GET /products with values', async () => {
+    const product = buildProduct()
+
+    const responsePost = await request(app)
+      .post('/products')
+      .send({
+        name: product.name,
+        size: product.size,
+        description: product.description,
+      })
+      .expect(201)
+
+    const { name, size, description, _id, __v } =
+      responsePost.body.productStored
+
+    const responseGet = await request(app).get('/products').expect(200)
+
+    expect(responseGet.body).toEqual({
+      products: [{ name, size, description, _id, __v }],
+    })
   })
 })
