f3335864fb6f9bf4f3b940cb90be262c310e9792
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-07-19 13:38:34 -0500

    validate required fields on signin

--- backend-testing-2/__tests__/auth/auth.integration.test.js
+++ backend-testing-2/__tests__/auth/auth.integration.test.js
@@ -18,8 +18,20 @@ afterAll(async () => {
 
 describe('auth integration tests', () => {
   test('success signin', async () => {
-    const response = await request(app).post('/signin').expect(200)
+    const response = await request(app)
+      .post('/signin')
+      .send({
+        email: 'john.doe@mail.com',
+        password: '123456789',
+      })
+      .expect(200)
 
     expect(response.body.message).toBe('success')
+  })
+
+  test('required email and password fields', async () => {
+    const response = await request(app).post('/signin').expect(400)
+
+    expect(response.body.message).toBe('Email and password are required')
   })
 })
--- backend-testing-2/src/controllers/auth-controllers.js
+++ backend-testing-2/src/controllers/auth-controllers.js
@@ -1,4 +1,10 @@
 module.exports.signin = (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(400).send({ message: 'Email and password are required' })
+  }
+
   res.status(200).send({ message: 'success' })
 }
 
