ceb9ab6ad42a7868974009f745777bece11fee92
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-07-19 14:32:57 -0500

    test unexpected error signin

--- backend-testing-2/src/controllers/auth-controllers.js
+++ backend-testing-2/src/controllers/auth-controllers.js
@@ -1,21 +1,27 @@
 const { findUserByEmail, createUser } = require('../data/user-data')
 
 module.exports.signin = async (req, res) => {
-  const { email, password } = req.body
+  try {
+    const { email, password } = req.body
 
-  if (!email || !password) {
-    return res.status(400).send({ message: 'Email and password are required' })
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: 'Email and password are required' })
+    }
+
+    const user = await findUserByEmail({ email })
+
+    if (user) {
+      return res.status(400).send({ message: 'user already exists' })
+    }
+
+    await createUser({ email, password })
+
+    res.status(200).send({ message: 'success' })
+  } catch (e) {
+    res.status(500).send({ message: 'There was an unexpected error' })
   }
-
-  const user = await findUserByEmail({ email })
-
-  if (user) {
-    return res.status(400).send({ message: 'user already exists' })
-  }
-
-  await createUser({ email, password })
-
-  res.status(200).send({ message: 'success' })
 }
 
 module.exports.login = (req, res) => {}
