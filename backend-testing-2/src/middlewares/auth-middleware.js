a29a0cb19fffd78b2cf463ff72af5e09fa3ca420
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-07-23 08:37:25 -0500

    added is auth handler middleware

---
+++ backend-testing-2/src/middlewares/auth-middleware.js
@@ -0,0 +1,27 @@
+const { verify } = require('../utils/jwt')
+
+module.exports.isAuthHandler = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res
+      .status(403)
+      .send({ message: 'The Authorization header was not sent' })
+  }
+
+  const [init, token] = req.headers.authorization.split(' ')
+
+  if (init !== 'Bearer') {
+    return res
+      .status(403)
+      .send({ message: 'The Authorization header does not contain Bearer' })
+  }
+
+  try {
+    const payloadDecoded = verify(token)
+
+    req.data.payload = payloadDecoded
+
+    next()
+  } catch (e) {
+    res.status(401).send({ message: 'Invalid token authentication' })
+  }
+}
