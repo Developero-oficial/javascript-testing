31f2160fd3c329a6471aaf9bd2b6c92c184434b8
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-07-31 15:15:45 -0500

    added test required login fields

--- frontend-testing-2/src/pages/login-page/login-page.int.test.js
+++ frontend-testing-2/src/pages/login-page/login-page.int.test.js
@@ -2,10 +2,16 @@ import {screen, render, fireEvent, waitFor} from '@testing-library/react'
 
 import {WithProviders} from '../../utils/test-utils'
 import {LoginPage} from '.'
+import {removeItem} from '../../utils/storage-utils'
 
 import {server} from '../../mocks/server.js'
 
 beforeAll(() => server.listen())
+
+beforeEach(() => {
+  removeItem({key: '@token'})
+  render(<LoginPage />, {wrapper: WithProviders})
+})
 
 afterEach(() => server.resetHandlers())
 
@@ -13,8 +19,6 @@ afterAll(() => server.close())
 
 describe('LoginPage Integration', () => {
   test('should succesfully login', async () => {
-    render(<LoginPage />, {wrapper: WithProviders})
-
     const emailInputEl = screen.getByLabelText(/email address/i)
     const passwordInputEl = screen.getByLabelText(/password/i)
     const submitBtnEl = screen.getByRole('button', {name: /send/i})
@@ -27,5 +31,17 @@ describe('LoginPage Integration', () => {
       expect(screen.queryByText(/login/i)).not.toBeInTheDocument()
     })
     expect(window.location.pathname).toBe('/dashboard')
+  })
+
+  test('should validate required fields', () => {
+    const submitBtnEl = screen.getByRole('button', {name: /send/i})
+
+    fireEvent.click(submitBtnEl)
+
+    const emailRequiredEl = screen.queryByText(/the email is required/i)
+    const passwordRequiredEl = screen.queryByText(/the password is required/i)
+
+    expect(emailRequiredEl).toBeInTheDocument()
+    expect(passwordRequiredEl).toBeInTheDocument()
   })
 })
