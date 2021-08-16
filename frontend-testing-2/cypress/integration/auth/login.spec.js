a2ac84bd38775f658f4dd38ad1cb9784aef1ad0c
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-08-16 12:50:56 -0500

    refactor using users fixture

--- frontend-testing-2/cypress/integration/auth/login.spec.js
+++ frontend-testing-2/cypress/integration/auth/login.spec.js
@@ -1,11 +1,17 @@
 /// <reference types="cypress" />
 
-describe('login e2e', () => {
-  it('should login successfully', () => {
-    cy.visit('http://localhost:3000')
+describe('login e2e', function () {
+  beforeEach(function () {
+    cy.fixture('users').then(users => {
+      this.admin = users.admin
+    })
+  })
 
-    cy.findByLabelText(/email address/i).type('john.doe@mail.com')
-    cy.findByLabelText(/password/i).type('123456')
+  it('should login successfully', function () {
+    cy.visit('/')
+
+    cy.findByLabelText(/email address/i).type(this.admin.email)
+    cy.findByLabelText(/password/i).type(this.admin.password)
     cy.findByRole('button', {name: /send/i}).click()
     cy.findByText(/Dashboard/i).should('exist')
   })
